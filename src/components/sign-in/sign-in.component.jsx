33ddc0f9de68066437004ef3af2aa309efe6cb1e
Ilyes Houha <mi.houha@gmail.com>
2021-01-12 23:51:31 +0100

    Fix FormInput

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { cacheNames } from "workbox-core/_private";
 
 import FormInput from "./../form-input/form-input.component";
 
@@ -21,30 +22,30 @@ class SignIn extends React.Component {
   };
 
   handleChange = (event) => {
-    const { value, name } = event;
-
+    const { value, name } = event.target;
     this.setState({ [name]: value });
   };
   render() {
     return (
       <div className="sign-in">
         <h2>I already have an account</h2>
-        <span>Sign in with your email</span>
+        <span>Sign in with your email and password</span>
+
         <form onSubmit={this.handleSubmit}>
           <FormInput
             name="email"
             type="email"
-            label="email"
-            value={this.state.email}
             handleChange={this.handleChange}
+            value={this.state.email}
+            label="email"
             required
           />
           <FormInput
             name="password"
             type="password"
-            label="password"
             value={this.state.password}
             handleChange={this.handleChange}
+            label="password"
             required
           />
           <input type="submit" value="Submit form" />
