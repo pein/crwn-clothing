5dfed51fc4150bdc0f444cd972f841469abac0c1
Ilyes Houha <mi.houha@gmail.com>
2021-01-21 04:13:03 +0100

    added cartIcon and cartDropdown componenets and implemented cart reducer

---
+++ src/redux/cart/cart.reducer.js
@@ -0,0 +1,20 @@
+import { UserActionTypes } from "./cart.types";
+
+const INITIAL_STATE = {
+  hidden: true,
+};
+
+const cartReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case UserActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
