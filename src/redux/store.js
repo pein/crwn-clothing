b6731fad81c2ebce04a3e37b1fc93b6a6e0c8b7b
Ilyes Houha <mi.houha@gmail.com>
2021-01-23 12:23:02 +0100

    Persist state to localstorage using redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,17 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
+export const store = createStore(
   rootReducer,
   /* preloadedState, */ composeEnhancers(applyMiddleware(...middlewares))
 );
+
+export const persistor = persistStore(store);
 
 export default store;
